cb9cd29b6a33deb7f471124e9e207a78fd7fc121
hshnj0216 <ldecena1116@gmail.com>
2023-04-07 01:03:22 +0800

    add data and template

--- djangonics/djangonics/settings.py
+++ djangonics/djangonics/settings.py
@@ -114,9 +114,11 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
+
+USE_THOUSAND_SEPARATOR = True
 
 
 # Static files (CSS, JavaScript, Images)
--- djangonics/products/views.py
+++ djangonics/products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Product, Category
 
 # Create your views here.
 def browse_all(request):
-    return render(request, 'products/browse_all.html')
+    products = Product.objects.all()
+    return render(request, 'products/browse_all.html', {'products': products})
