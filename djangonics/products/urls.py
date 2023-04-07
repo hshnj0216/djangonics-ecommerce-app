5888643c98aeaa9767ba27c20699f56c83b7b037
hshnj0216 <ldecena1116@gmail.com>
2023-04-08 01:08:26 +0800

    improve UI

--- djangonics/products/urls.py
+++ djangonics/products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'products'
+
 urlpatterns = [
     path('', views.browse_all, name='browse_all'),
+    path('<slug:slug>/', views.product_details, name='product_details'),
 ]
--- djangonics/products/views.py
+++ djangonics/products/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 # Create your views here.
 def browse_all(request):
     products = Product.objects.all()
     return render(request, 'products/browse_all.html', {'products': products})
+
+def product_details(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'products/product_details.html', {'product': product})
