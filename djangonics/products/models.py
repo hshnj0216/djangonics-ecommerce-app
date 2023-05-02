273f0c7b8e6ada96989d748b7c5d763d65a114b4
hshnj0216 <ldecena1116@gmail.com>
2023-05-02 13:45:28 +0800

    improve product cards and add models

--- djangonics/products/models.py
+++ djangonics/products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.contrib.postgres.search import SearchVector, SearchVectorField
+from accounts.models import User
 
 
 # Create your models here.
@@ -49,3 +49,20 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey('Cart', on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('cart', 'product')
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.quantity * self.product.price
+        super().save(*args, **kwargs)
